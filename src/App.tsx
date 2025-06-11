fbb61e8ea0a60a260381943ea5b42d9de9631c5f
Lakshya Bansal <lakshyab9712@gmail.com>
2025-06-11 15:53:58 +0530

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import AdminPanel from './pages/adminPage';
 import Login from './auth/login';
 import Signup from './auth/singup';
 import LandingPage from './pages/LandingPage';
-import ResetPassword from './auth/ResetPassword';
-import UpdatePassword from './auth/UpdatePassword';
 function App() {
   return (
     <Router>
@@ -15,8 +13,6 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/user" element={<UserPanel />} /> {/* Moved UserPanel to /user */}
         <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/reset-Password" element={<ResetPassword />} />
-        <Route path="/update-password" element={<UpdatePassword />} />
       </Routes>
     </Router>
   );
